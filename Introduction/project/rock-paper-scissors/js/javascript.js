ad6cfd93fdc4be452a8d9957deb19571be80837f
AthillaZaidan <athillazaidanstudy@gmail.com>
2025-06-25 20:37:49 +0700

    rps done

--- Introduction/project/rock-paper-scissors/js/javascript.js
+++ Introduction/project/rock-paper-scissors/js/javascript.js
@@ -1,82 +1,130 @@
-// /*
-// *
-// * rock paper scissors
-// * rock = 0
-// * paper = 1
-// * scissors = 2
-// * batu > gunting
-// * gunting > kertas
-// * kertas > batu
-// *
-// */
-// function getComputerChoice(){
-//     return Math.floor(Math.random() * 3);
-// }
-
-// function getHumanChoice(){
-//     let option = prompt("input (rock/paper/scissors)");
-//     option = option.toLowerCase();
-//     if (option == "rock"){
-//         return 0;
-//     } else if (option == "paper"){
-//         return 1;
-//     } else if (option == "scissors"){
-//         return 2;
-//     } else {
-//         alert("Pilihan tidak valid. Gunakan: rock, paper, atau scissors.");
-//         return getHumanChoice(); 
-//     }
-// }
-
-// let humanScore = 0;
-// let computerScore = 0;
-
-// function playRound(humanChoice, computerChoice){
-//     if (humanChoice == 0 && computerChoice == 2){
-//         humanScore++;
-//         alert("MANUSIA MENANG");
-//     } else if (humanChoice == 2 && computerChoice == 0){
-//         computerScore++;
-//         alert("ROBOT MENANG");
-//     } else if (humanChoice == 2 && computerChoice == 1){
-//         humanScore++;
-//         alert("MANUSIA MENANG");
-//     } else if (humanChoice == 1 && computerChoice == 2){
-//         computerScore++;
-//         alert("ROBOT MENANG");
-//     } else if (humanChoice == 1 && computerChoice == 0){
-//         humanScore++;
-//         alert("MANUSIA MENANG");
-//     } else if (humanChoice == 0 && computerChoice == 1){
-//         computerScore++;
-//         alert("ROBOT MENANG");
-//     } else {
-//         alert("DRAW");
-//         return "DRAW";
-//     }
-//     return 0;
-// }
+/*
+*
+* rock paper scissors
+* rock = 0
+* paper = 1
+* scissors = 2
+* batu > gunting
+* gunting > kertas
+* kertas > batu
+*
+*/
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
 
 
+let humanScore = 0;
+let computerScore = 0;
 
-// function playGame(){
-//     let i = 0;
-//     while (i < 5){
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         let hasil = playRound(humanSelection, computerSelection);
-//         i++;
-//         if (hasil == "DRAW"){
-//             i--;
-//         }
-//     }
-//     if (humanScore > computerScore){
-//         console.log("MANUSIA MENANG");
-//     } else {
-//         console.log("ROBOT MENANG");
-//     }
-//     console.log(humanScore);
-//     console.log(computerScore);
-// }
+function updateScoreUI() {
+    playerScoreElement.textContent = "Player: " + humanScore;
+    computerScoreElement.textContent = "Computer: " + computerScore;
+}
 
-// playGame(); 
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == 0 && computerChoice == 2){
+        humanScore++;
+        updateScoreUI();
+        return true;
+    } else if (humanChoice == 2 && computerChoice == 0){
+        computerScore++;
+        updateScoreUI();
+        return false;
+    } else if (humanChoice == 2 && computerChoice == 1){
+        humanScore++;
+        updateScoreUI();
+        return true;
+    } else if (humanChoice == 1 && computerChoice == 2){
+        computerScore++;
+        updateScoreUI();
+        return false;
+    } else if (humanChoice == 1 && computerChoice == 0){
+        humanScore++;
+        updateScoreUI();
+        return true;
+    } else if (humanChoice == 0 && computerChoice == 1){
+        computerScore++;
+        updateScoreUI();
+        return false;
+    } else {
+        return "DRAW";
+    }
+}
+
+const bodyContainer = document.querySelector(".body");
+const wordSelector = document.getElementById("word");
+const buttonDivSelector = document.getElementById("button");
+const buttonSelector = document.getElementById("play-btn");
+
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
+
+buttonSelector.onclick = playGame;
+
+let isPlaying = false;
+
+function playGame(){
+    if (isPlaying) return;
+    isPlaying = true;
+    const currentWord = document.getElementById("word");
+    currentWord.textContent = "Choose Your Move!";
+    const currentButtonDiv = document.getElementById("button");
+    if (currentButtonDiv) currentButtonDiv.remove();
+    const button1 = document.createElement("button");
+    button1.textContent = "🪨 ROCK";
+    button1.setAttribute("id", "button1");
+    const button2 = document.createElement("button");
+    button2.textContent = "📄 PAPER";
+    button2.setAttribute("id", "button2");
+    const button3 = document.createElement("button");
+    button3.textContent = "✂️ SCISSORS";
+    button3.setAttribute("id", "button3");
+    bodyContainer.append(button1);
+    bodyContainer.append(button2);
+    bodyContainer.append(button3);
+    let computerChoice = getComputerChoice();
+    button1.onclick = function() {
+        handleResult(0, computerChoice);
+    };
+    button2.onclick = function() {
+        handleResult(1, computerChoice);
+    };
+    button3.onclick = function() {
+        handleResult(2, computerChoice);
+    };
+}
+
+function handleResult(humanChoice, computerChoice) {
+    const result = playRound(humanChoice, computerChoice);
+    document.getElementById("word").remove();
+    document.getElementById("button1").remove();
+    document.getElementById("button2").remove();
+    document.getElementById("button3").remove();
+    const winDeclaration = document.createElement("div");
+    winDeclaration.setAttribute("id", "word");
+    if (result === true) {
+        winDeclaration.textContent = "🎉🎉SELAMAT KAMU MENANG😁😁";
+    } else if (result === false) {
+        winDeclaration.textContent = "💔💔DAMN KAMU KALAH😈😈";
+    } else {
+        winDeclaration.textContent = "DRAW";
+    }
+    bodyContainer.append(winDeclaration);
+    setTimeout(function() {
+        bodyContainer.innerHTML = "";
+        const wordDiv = document.createElement("div");
+        wordDiv.id = "word";
+        wordDiv.textContent = "Click This Button to Play";
+        const buttonDiv = document.createElement("div");
+        buttonDiv.id = "button";
+        const playBtn = document.createElement("button");
+        playBtn.id = "play-btn";
+        playBtn.textContent = "Click Me!";
+        buttonDiv.appendChild(playBtn);
+        bodyContainer.appendChild(wordDiv);
+        bodyContainer.appendChild(buttonDiv);
+        playBtn.onclick = playGame;
+        isPlaying = false;
+    }, 2000);
+}
